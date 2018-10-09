60eec7f77f9aee16221d272d27b4a799455351ee
Tania Whiting <tanwhiting@aol.com>
2018-10-09 22:37:40 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -37,8 +37,7 @@ end
 def add_s(array)
   array.each_with_index.collect do |element, index|
     if index != 1 
-      element << "s"
-    end  
+      element= element.push("s")
   end
   return array
 end
